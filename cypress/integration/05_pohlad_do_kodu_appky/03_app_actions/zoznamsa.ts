11ade0b3d2e7d7089e90e38d15de6113a440603c
Filip Hric <filip@filiphric.com>
2021-09-02 16:18:21 +0200

    fix typo

--- cypress/integration/05_pohlad_do_kodu_appky/03_app_actions/zoznamsa.ts
+++ cypress/integration/05_pohlad_do_kodu_appky/03_app_actions/zoznamsa.ts
@@ -1,12 +1,10 @@
-import { store } from "trelloapp/src/stores/store";
-
 // úloha #1: skús si pomocou app action otvoriť api nástroje
 it('otvorenie api nástrojov', () => {
 
   cy
     .visit('/')
 
-  cy.window().invoke('store').then( store => {
+  cy.window().invoke('store').then( (store) => {
     store.showTools = true;
   })
 
